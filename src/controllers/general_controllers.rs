3e9e8696e38596c39b7e2382b01e8645092bd592
Ahmad Ali Abdilah <ahmad.ali.abdilah@gmail.com>
2023-01-08 00:12:30 +0700

    remove unused import

--- src/controllers/general_controllers.rs
+++ src/controllers/general_controllers.rs
@@ -1,5 +1,5 @@
 use actix_web::http::StatusCode;
-use actix_web::{web, Error, HttpResponse};
+use actix_web::{Error, HttpResponse};
 use anyhow::Result;
 
 pub async fn index() -> Result<HttpResponse, Error> {
